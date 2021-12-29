824593746cd201d51c8e872260ac69a496310a4d
tharnob <tharnob@gmail.com>
2021-12-29 12:11:03 +0600

    increment project commited

---
+++ increment.js
@@ -0,0 +1,37 @@
+const domContainer = document.querySelector("#root");
+
+const Increment = () => {
+    const [counter, setCounter] = React.useState(0);
+    return (
+        <div className="elements">
+            <h1 id="display">{ counter }</h1>
+            <div>
+                <button id="button" onClick={() => setCounter(counter+1)}>Increment +</button>
+            </div>
+        </div>
+    );
+}
+
+
+
+ReactDOM.render(
+    
+    <div className = "container">
+        <Increment/>
+        <Increment/>
+        <Increment/>
+
+    </div>,
+    domContainer);
+
+
+// let number = 0;
+// const display = document.querySelector("#display");
+// const button = document.querySelector("#button");
+
+
+// button.addEventListener("click" , () => {
+//     number++;
+//     display.textContent = number;
+// });
+
